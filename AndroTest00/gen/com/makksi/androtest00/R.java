6454aa6dab255b8b4b3bd2d352ee190cd85588e5
makksi <makksim@hotmail.com>
2013-02-19 14:41:27 +0100

    updated resource files

--- AndroTest00/gen/com/makksi/androtest00/R.java
+++ AndroTest00/gen/com/makksi/androtest00/R.java
@@ -15,11 +15,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int loadButton=0x7f070002;
-        public static final int menu_settings=0x7f070004;
-        public static final int saveButton=0x7f070001;
-        public static final int textArea=0x7f070003;
-        public static final int textView1=0x7f070000;
+        public static final int loadButton=0x7f070001;
+        public static final int menu_settings=0x7f070003;
+        public static final int saveButton=0x7f070000;
+        public static final int textArea=0x7f070002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -31,7 +30,9 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int app_test=0x7f040001;
         public static final int hello_world=0x7f040002;
+        public static final int loadButtonText=0x7f040006;
         public static final int menu_settings=0x7f040003;
+        public static final int saveButtonText=0x7f040005;
         public static final int title_activity_main=0x7f040004;
     }
     public static final class style {
